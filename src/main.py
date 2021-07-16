264dc8557ca7f3eb0de1d927f3eec5e709f7eb1d
xenogenesis20 <xenogenesis20@outlook.com>
2021-07-16 23:50:45 +0000

    backend done

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User,Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -32,12 +32,80 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
-
+    all_users = User.query.all()
+    all_users = list(map(lambda user: user.serialize(), all_users))
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "users": all_users
     }
 
     return jsonify(response_body), 200
+
+@app.route('/<user>/favorites', methods=['GET'])
+def get_Favorites(user):
+    single_user = User.query.filter_by(user_name=user).first()
+    if single_user is None:
+        raise APIException("User not found", status_code=404)
+    favorites = Favorite.query.filter_by(user_name=user)
+    all_favorites = list(map(lambda fav: fav.serialize(), favorites))
+    response_body = {
+        "All Favorites": all_favorites
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/favorite/planet/<int:id>', methods=['POST','DELETE'])
+def add_planet(id):
+    request_body = request.get_json()
+    if request_body is None or request_body =={}:
+        raise APIException("Body cannot be empty", status_code=404)
+    if request.method == 'POST':
+        new_planet = Favorite(name=request_body["name"], entity_type="planet",entity_id=id,user_name=request_body["user_name"])
+        db.session.add(new_planet)
+        db.session.commit()
+    if request.method == 'DELETE':
+        deleted_planet = Favorite.query.filter_by(entity_type="planet",entity_id=id,user_name=request_body["user_name"]).first()
+        if deleted_planet is None:
+            raise APIException("Planet not found", status_code=404)
+        db.session.delete(deleted_planet)
+        db.session.commit()
+    favorites = Favorite.query.filter_by(user_name=request_body["user_name"])
+    all_favorites = list(map(lambda fav: fav.serialize(), favorites))
+    response_body = {
+        "Favorites": all_favorites 
+    }
+    return jsonify(response_body), 200
+
+@app.route('/favorite/person/<int:id>', methods=['POST','DELETE'])
+def add_person(id):
+    request_body = request.get_json()
+    if request_body is None or request_body =={}:
+        raise APIException("Body cannot be empty", status_code=404)
+    if request.method == 'POST':
+        person = Favorite.query.filter_by(user_name=request_body["user_name"],name = request_body["name"]).first()
+        if person:
+            raise APIException("Favorite already exists", status_code=418)
+        new_person = Favorite(name=request_body["name"], entity_type="person",entity_id=id,user_name=request_body["user_name"])
+        db.session.add(new_person)
+        db.session.commit()
+    if request.method == 'DELETE':
+        deleted_person = Favorite.query.filter_by(entity_type="person",entity_id=id,user_name=request_body["user_name"]).first()
+        if deleted_person is None:
+            raise APIException("person not found", status_code=404)
+        db.session.delete(deleted_person)
+        db.session.commit()
+    favorites = Favorite.query.filter_by(user_name=request_body["user_name"])
+    all_favorites = list(map(lambda fav: fav.serialize(), favorites))
+    response_body = {
+        "Favorites": all_favorites 
+    }
+    return jsonify(response_body), 200
+
+
+
+
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,16 +4,37 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(20), unique = True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship('Favorite', backref='user')
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r,%r,%r,%r>' % (self.id,self.user_name,self.email,self.password)
 
     def serialize(self):
         return {
             "id": self.id,
+            "user_name": self.user_name,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "favorites":[favorite.serialize() for favorite in self.favorites]
+        }
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=False, nullable=False)
+    entity_type = db.Column(db.String(255), unique=False, nullable=False)
+    entity_id = db.Column(db.Integer, unique=False, nullable=False)
+    user_name = db.Column(db.String(255), db.ForeignKey('user.user_name'))
+    
+    def __repr__(self):
+        return '<Favorite %r,%r,%r,%r,%r>' % (self.id,self.name,self.entity_type,self.entity_id,self.user_name)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "entity_type": self.entity_type,
+            "entity_id": self.entity_id,
+            "user_name": self.user_name,
         }
