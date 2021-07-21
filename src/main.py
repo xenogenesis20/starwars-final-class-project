712fadbc679a6622c5c6101b8fdf2033c86e1c76
xenogenesis20 <xenogenesis20@outlook.com>
2021-07-21 22:47:45 +0000

    here

--- src/main.py
+++ src/main.py
@@ -9,6 +9,11 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User,Favorite
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+import datetime
 #from models import Person
 
 app = Flask(__name__)
@@ -19,6 +24,9 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this "super secret" with something else!
+jwt = JWTManager(app)
+
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -29,6 +37,22 @@ def handle_invalid_usage(error):
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
+@app.route("/login", methods=["POST"])
+def create_token():
+    credentials = request.get_json()
+    username = credentials.get("username", None)
+    password = credentials.get("password", None)
+    # Query your database for username and password
+    user = User.query.filter_by(user_name=username, password=password).first()
+    if user is None:
+        # the user was not found on the database
+        return jsonify({"msg": "Invalid username or password"}), 401
+    
+    # create a new token with the user id inside
+    expires = datetime.timedelta(days=7)
+    access_token = create_access_token(identity=user.user_name,expires_delta=expires)
+    return jsonify({ "token": access_token, "user_id": user.user_name })    
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
@@ -48,7 +72,7 @@ def get_Favorites(user):
     favorites = Favorite.query.filter_by(user_name=user)
     all_favorites = list(map(lambda fav: fav.serialize(), favorites))
     response_body = {
-        "All Favorites": all_favorites
+        "all_favorites": all_favorites
     }
 
     return jsonify(response_body), 200
@@ -71,7 +95,7 @@ def add_planet(id):
     favorites = Favorite.query.filter_by(user_name=request_body["user_name"])
     all_favorites = list(map(lambda fav: fav.serialize(), favorites))
     response_body = {
-        "Favorites": all_favorites 
+        "all_favorites": all_favorites 
     }
     return jsonify(response_body), 200
 
@@ -96,7 +120,7 @@ def add_person(id):
     favorites = Favorite.query.filter_by(user_name=request_body["user_name"])
     all_favorites = list(map(lambda fav: fav.serialize(), favorites))
     response_body = {
-        "Favorites": all_favorites 
+        "all_favorites": all_favorites 
     }
     return jsonify(response_body), 200
 
